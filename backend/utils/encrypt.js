2027cd437d56fc35390b497dd2e8dc9079dfee6a
mridul51204 <mridulgrover0@gmail.com>
2025-11-10 15:02:34 +0530

    Update encrypt.js

--- backend/utils/encrypt.js
+++ backend/utils/encrypt.js
@@ -1,8 +1,7 @@
 import CryptoJS from "crypto-js";
 
-// Encrypt a file buffer using AES-256
 export function encryptFile(buffer) {
-  const key = CryptoJS.lib.WordArray.random(32);  // 256-bit key
+  const key = CryptoJS.lib.WordArray.random(32);  // 256-bit AES key
   const iv = CryptoJS.lib.WordArray.random(16);   // 128-bit IV
   const wordArray = CryptoJS.lib.WordArray.create(buffer);
   const encrypted = CryptoJS.AES.encrypt(wordArray, key, { iv });
